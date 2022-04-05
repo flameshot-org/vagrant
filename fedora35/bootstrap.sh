fb48da105aa214af31d09c92c9446548f450133c
borgmanJeremy <46930769+borgmanJeremy@users.noreply.github.com>
2022-04-05 14:45:48 -0500

    added fedora builds

---
+++ fedora35/bootstrap.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# Configure Base VM
+dnf update
+dnf upgrade -y
+dnf install -y @base-x gnome-shell gnome-terminal nautilus spice-vdagent chrome-gnome-shell gnome-tweaks @development-tools
+systemctl set-default graphical.target
+
+# Add flameshot build dependencies
+dnf install -y gcc-c++ cmake qt5-qtbase-devel qt5-linguist ninja-build git
+
+# Add flameshot runtime dependencies
+dnf install -y qt5-qtbase qt5-qtsvg-devel
+
+# Default to package manager Flameshot version
+dnf install -y flameshot
+
+
+# Download flameshot source, build flameshot, but do not install globally
+cd /home/vagrant/
+git clone https://github.com/flameshot-org/flameshot.git
+cd flameshot
+mkdir build
+cd build
+cmake -GNinja ../ 
+ninja
+
